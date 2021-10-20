449b3392beee4006f1700e0d6b660800a7c332cd
Sami Spets <34544461+spetssami@users.noreply.github.com>
2021-10-20 22:47:43 +0300

    Create risseSkripti.js

---
+++ risseSkripti.js
@@ -0,0 +1,33 @@
+let jokuBoolean = true
+const nappula = document.querySelectorAll("button")[5];
+
+function paivita(){
+	if(jokuBoolean){
+    if(document.getElementsByClassName("o-align-items--flex-start").length != 0){
+				let omalista = document.querySelectorAll(".o-align-items--flex-start")
+				for(let i=0; i<omalista.length; i++){
+					if(omalista[i].innerText.includes("E4")){
+						omalista[i].click()
+						console.log("piippiip")
+						jokuBoolean = false
+						continue;
+				}
+			}
+		} else {
+			painaNappulaa()
+		}
+	}
+}
+
+function painaNappulaa(){
+	nappula.click()
+}
+
+window.addEventListener("keydown", (event) => {
+    if (event.defaultPrevented) {
+        return;
+    }
+    if (event.keyCode == 39){ //Oikea nuolinäppäin
+        paivita()
+    }
+})
